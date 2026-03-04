83cf2d5cd5d55b808eddd0851921e7e795d70e19
Noam Lemel <noam123435@gmail.com>
2026-03-04 20:26:28 +0200

    drill 2 fixed

--- src/advance-js-string.js
+++ src/advance-js-string.js
@@ -32,9 +32,37 @@ let strIn="aac";
 console.log(myIncludes2(strOut,strIn));
 
 //drill 02
+//my fixed solution
 const myIndexOf=(str,strSearched,position=0)=>{
-    for(let i=position;i<str.length-strSearched.length;i++){
-        
+    for(let i=position;i<str.length;i++){
+        for(let j=0;j<strSearched.length;j++){
+            if(str[i+j]!==strSearched[j]){
+                break;
+            }
+            if(j===strSearched.length-1)
+                return i;
+        }
     }
-
+    return -1;
 }
+strOut="HHHHHGGFDaaac";
+strIn="aac";
+console.log(myIndexOf(strOut,strIn));
+
+//Arie's Solution
+const myIndexOf2=(str,strSearched,position=0)=>{
+    let i2=0;
+    for(let i=position;i<str.length;i++){
+        if(str[i]!==strSearched[i2]) {
+            i-=i2;
+            i2=0;
+            continue;
+        }
+        if(i2===strSearched.length-1) return i-i2;
+        i2++
+    }
+return -1;
+}
+strOut="HHHHHGGFDaaac";
+strIn="aac";
+console.log(myIndexOf2(strOut,strIn));

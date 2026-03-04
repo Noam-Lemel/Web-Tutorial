131de631177dd9828e9661af2e67ea340005a624
Noam Lemel <noam123435@gmail.com>
2026-03-04 18:56:24 +0200

    drill 01

---
+++ src/advance-js-string.js
@@ -0,0 +1,32 @@
+//drill 01
+// my Fixed solution
+const myIncludes=(str,strSearched,position=0)=>{
+    for(let i=position;i<str.length;i++){
+        let found=true;
+        for(let j=0;j<strSearched.length-strSearched.length;j++){
+            if(str[i+j]!== strSearched[j]){
+                found=false;
+                break;
+            }
+        }
+        if(found) return true;
+    }
+    return false;
+}
+// Arie's Solution
+const myIncludes2=(str,strSearched,position=0)=>{
+    let i2=0;
+    for(let i=position;i<str.length;i++){
+        if(str[i]!==strSearched[i2]) {
+            i-=i2;
+            i2=0;
+            continue;
+        }
+        if(i2===strSearched.length-1) return true;
+        i2++
+    }
+return false;
+}
+let strOut="aaac";
+let strIn="aac";
+console.log(myIncludes2(strOut,strIn));
